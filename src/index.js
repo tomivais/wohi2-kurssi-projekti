e402786eded96090cdb56ab0acbc40885dc27cd2
tomivais <128896792+tomivais@users.noreply.github.com>
2026-04-15 11:35:08 +0300

    Index.js:n middlewareen päivitys

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ const express=require('express');
 const app = express();
 const quizsRouter = require("./routes/quizs") //Tuodaaan reitit
 const prisma = require("./lib/prisma"); // Tuodaan Prisma-yhteys
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // Määritetään portti
 
 app.use(express.json());
 app.use("/api/quizs", quizsRouter);
 app.get("/", (req, res) => {
-    res.status(404).json({message: "Not found"});
+    res.status(404).json({
+        message: "Not found"});
 });
-
 
 
 app.listen(PORT,()=>{
